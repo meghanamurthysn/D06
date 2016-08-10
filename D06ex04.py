4844fddf2c765dc35127a9ab02f6e84604c544b0
meghanamurthysn <meghanamurthy@berkeley.edu>
2016-08-10 06:25:55 -0700

    D06ex04.py_v01

---
+++ D06ex04.py
@@ -0,0 +1,12 @@
+def print_name_e():
+	fin = open('roster.txt','r')
+	fin1 = open('Names with e.txt', 'w')
+	for name in fin:
+		if 'e' in name.split()[0]:
+			fin1.write(name)
+#############################################################################
+def main():
+	print_name_e()
+
+if __name__ == '__main__':
+	main()
