e7432008dd0de09778bc865fef71f75429563091
meghanamurthysn <meghanamurthy@berkeley.edu>
2016-08-10 06:28:49 -0700

    HW06_ch09_ex05.py_v01

--- HW06_ch09_ex05.py
+++ HW06_ch09_ex05.py
@@ -10,17 +10,34 @@
 # How many words are there that use all the vowels aeiou? How about
 # aeiouy?
 #   - write functions(s) to assist you
-#   - # of words that use all aeiou: [type here]
-#   - # of words that use all aeiouy: [type here]
+#   - # of words that use all aeiou: 598
+#   - # of words that use all aeiouy: 42
 ##############################################################################
 # Imports
 
 # Body
+def uses_all(word, str_inp):
+	for ltr in str_inp:
+		if ltr not in word:
+			return False
+	return True
 
+def count_uses(fin, str_inp):
+	count = 0
+	for word in fin:
+		if uses_all(word, str_inp):
+			count += 1
+	return(count)
+
+def read_file():
+	fin = open('words.txt','r')
+	print("Number of words that use all aeiou: {}".format(count_uses(fin, 'aeiou')))
+	fin.seek(0,0)
+	print("Number of words that use all aeiouy: {}".format(count_uses(fin, 'aeiouy')))
 
 ##############################################################################
 def main():
-    pass  # Call your function(s) here.
+    read_file()  # Call your function(s) here.
 
 if __name__ == '__main__':
     main()
