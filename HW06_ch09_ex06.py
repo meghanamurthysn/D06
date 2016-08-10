9461ab540639762049515c60b3f6e1a5c99af938
meghanamurthysn <meghanamurthy@berkeley.edu>
2016-08-10 06:30:10 -0700

    HW06_ch09_ex06.py_v01

--- HW06_ch09_ex06.py
+++ HW06_ch09_ex06.py
@@ -8,16 +8,34 @@
 # (2)
 # How many abecedarian words are there?
 #   - write additional function(s) to assist you
-#   - number of abecedarian words:
+#   - number of abecedarian words: 0
 ##############################################################################
 # Imports
 
 # Body
+def is_abecedarian(word):
+	count = 0
+	for ltr in word:
+		count += 1
+		if(count == 1):
+			index_alph = ord(ltr)
+		elif(index_alph > ord(ltr)):
+			return False
+		else:
+			index_alph = ord(ltr)
+	return True
 
+def num_abecedarian():
+	fin = open('words.txt','r')
+	num_abecedarian_words = 0
+	for word in fin:
+		if is_abecedarian(word):
+			num_abecedarian_words += 1
+	print("The number of abecedarian words in the file are: {}".format(num_abecedarian_words))
 
 ##############################################################################
 def main():
-    pass  # Call your function(s) here.
+    num_abecedarian()
 
 if __name__ == '__main__':
     main()
